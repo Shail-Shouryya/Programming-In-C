c001683324a44d31a7cc04dd2146f9125895d7b5
Shail-Shouryya <shailshouryya@gmail.com>
2019-05-13 23:16:23 -0700

    renamed files to more descriptive names

---
+++ Programs/03_input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("This sentence will print out exactly as you see it...\a\n");
+    
+    int a;
+    
+    printf("Please input an integer value: ");
+    scanf("%s", &a);
+    printf("You entered: %d\n", a);
+    
+    
+}
--- Programs/throwaway.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("This sentence will print out exactly as you see it...");
-}
